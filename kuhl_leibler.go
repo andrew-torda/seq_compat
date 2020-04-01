731dad93243b8cc9b6a255cde0c899e4369529d9
Andrew E. Torda <torda@zbh.uni-hamburg.de>
2020-04-01 17:33:42 +0200

    Starting

---
+++ kuhl_leibler.go
@@ -0,0 +1,11 @@
+// 1 Apr 2019
+// Read a sequence alignment. Take an arbitrary sequence as the one to
+// be checked for compatibility using the Kuhlbach-Leibler divergence.
+
+import (
+	"github.com/andrew-torda/goutil/seq"
+)
+
+package main
+
+
