26e86265161d07b4d25ae9dc897e6ea4999b0a42
Andrew E. Torda <torda@zbh.uni-hamburg.de>
2020-04-30 17:08:35 +0200

    Added TestEntropy and TestNdx.

--- seq/seq_test.go
+++ seq/seq_test.go
@@ -65,7 +65,7 @@ func writeTest_nospaces(f_tmp *os.File) {
 func innerWriteReadSeqs(t *testing.T, spaces int) {
 	f_tmp, err := ioutil.TempFile("", "_del_me_testing")
 	if err != nil {
-		t.Fatalf("tempfile %v: %s", f_tmp, err)
+		t.Fatal("tempfile", f_tmp, err)
 	}
 	defer f_tmp.Close()
 	switch spaces {
@@ -82,7 +82,7 @@ func innerWriteReadSeqs(t *testing.T, spaces int) {
 	var names = []string{f_tmp.Name()}
 	seqgrp, n_dup, err := Readfiles(names, s_opts)
 	if err != nil {
-		t.Fatalf("Reading seqs failed %v", err)
+		t.Fatal("Reading seqs failed", err)
 	}
 	if seqgrp.GetNSeq() != len(seq_lengths) {
 		t.Fatalf("Wrote %d seqs, but read only %d.\n%s, %d",
@@ -100,7 +100,6 @@ func innerWriteReadSeqs(t *testing.T, spaces int) {
 
 }
 
-func breaker() {}
 // Check that broken files are gracefully handled
 func TestEmpty(t *testing.T) {
 	bad_contents := []string{
@@ -111,17 +110,17 @@ func TestEmpty(t *testing.T) {
 	for _, content := range bad_contents {
 		f_tmp, err := ioutil.TempFile("", "_del_me_testing")
 		if err != nil {
-			t.Fatalf("tempfile %v: %s", f_tmp, err)
+			t.Fatal("tempfile", f_tmp, err)
 		}
 		defer os.Remove(f_tmp.Name())
 		if _, err := io.WriteString(f_tmp, content); err != nil {
-			t.Fatalf("writing string to temp file")
+			t.Fatal("writing string to temp file")
 		}
 
 		f_tmp.Close()
 		s_opts := &Options{Vbsty: 0, Keep_gaps_rd: true, Dry_run: true}
 		if _, _, err := Readfile(f_tmp.Name(), s_opts); err == nil {
-			t.Fatalf("should generate error on zero-length file")
+			t.Fatal("should generate error on zero-length file")
 		}
 	}
 
@@ -154,20 +153,21 @@ var stypedata = []struct {
 }
 
 func TestTypes(t *testing.T) {
-	s_opts := &Options{
+	var s_opts = &Options{
 		Vbsty: 0, Keep_gaps_rd: false,
 		Dry_run:      true,
 		Rmv_gaps_wrt: true,
 	}
+
 	for tnum, x := range stypedata {
 		f_tmp, err := ioutil.TempFile("", "_del_me_testing")
 		if err != nil {
-			t.Fatalf("tempfile %v: %s", f_tmp, err)
+			t.Fatal("tempfile", f_tmp, err)
 		}
 		defer os.Remove(f_tmp.Name())
 
 		if _, err := io.WriteString(f_tmp, x.s1); err != nil {
-			t.Fatalf("writing string to temp file")
+			t.Fatal("writing string to temp file")
 		}
 		f_tmp.Close()
 		seqgrp, _, err := Readfile(f_tmp.Name(), s_opts)
@@ -177,28 +177,153 @@ func TestTypes(t *testing.T) {
 			t.Fatalf("seq num %d (from 0) got %d expected %d", tnum, st, x.stype)
 		}
 	}
-
 }
 
-func TestCnt(t *testing.T) {
-	testseqs := `>s1
-ACDaa
->s2
-CCD-a
+// roughEql says if two numbers are roughly the same
+func roughEql(a, b float32) bool {
+	const eps float32 = 0.000001
+	d := a - b
+	if d < 0 {
+		d = -d
+	}
+	if d < eps {
+		return true
+	}
+	return false
+}
+
+// sliceEql returns true if two slices are roughly equal
+func sliceEql(a, b []float32) bool {
+	for i := range a {
+		if roughEql(a[i], b[i]) == false {
+			return false
+		}
+	}
+	return true
+}
+
+var entdata = []struct {
+	s1         string
+	gapAsChar  []float32 // If gaps are characters
+	gapNotChar []float32 // and if they are not counted
+}{
+	{`> s1'
+A
+> s2
+C
 > s3
-CCQaa`
-	s_opts := &Options{Vbsty: 0, Keep_gaps_rd: true, Dry_run: true}
+G
+> s4
+T
+> s5
+-`,
+		[]float32{1},
+		[]float32{1}},
+	{`> s1
+AAAA
+> s2
+AACT
+> s3
+ACG-
+`,
+		[]float32{0.0, 0.39548847, 0.6826062, 0.6826062},
+		[]float32{0.0, 0.459147917027, 0.792481250362, 0.5},
+	},
+	{`> s1
+AAAAA
+> s2
+AADAC
+> s3
+AEG--`,
+		[]float32{0, 0.20906864, 0.3608488, 0.20906864, 0.3608488},
+		[]float32{0, 0.21247365, 0.3667258, 0, 0.23137821316},
+	},
+}
+
+func wrtTmp(s string) (string, error) {
 	f_tmp, err := ioutil.TempFile("", "_del_me_testing")
 	if err != nil {
-		t.Fatalf("tempfile %v: %s", f_tmp, err)
+		return "", fmt.Errorf("tempfile fail")
 	}
-	defer os.Remove(f_tmp.Name())
-	if _, err := io.WriteString(f_tmp, testseqs); err != nil {
-		t.Fatalf("writing string to temp file")
-	}
-	f_tmp.Close()
 
-	seqgrp, _, err := Readfile(f_tmp.Name(), s_opts)
-	seqgrp.Upper()
-	seqgrp.UsageFrac(false)
+	if _, err := io.WriteString(f_tmp, s); err != nil {
+		return "", fmt.Errorf("writing string to temp file %v", f_tmp.Name())
+	}
+	name := f_tmp.Name()
+	f_tmp.Close()
+	return name, nil
+}
+
+func TestEntropy(t *testing.T) {
+	s_opts := &Options{
+		Vbsty: 0, Keep_gaps_rd: true,
+		Dry_run:      true,
+		Rmv_gaps_wrt: false}
+
+	for tnum, x := range entdata {
+		var tmpname string
+		var err error
+		var entrpy []float32
+		var seqgrp SeqGrp
+		if tmpname, err = wrtTmp(x.s1); err != nil {
+			t.Fatal("tempfile error:", err)
+		}
+		defer os.Remove(tmpname)
+		if seqgrp, _, err = Readfile(tmpname, s_opts); err != nil {
+			t.Fatal("Test: ", tnum, err)
+		}
+		seqgrp.Upper()
+		if entrpy, err = seqgrp.Entropy(true); err != nil {
+			t.Fatal("entropy fail on sequence", tnum)
+		}
+
+		if !sliceEql(entrpy, x.gapAsChar) {
+			t.Fatal("set ", tnum, "gapaschar wanted\n", x.gapAsChar, "got\n", entrpy)
+		} // now do the negative case
+
+		seqgrp.Clear()
+		if entrpy, err = seqgrp.Entropy(false); err != nil {
+			t.Fatal("entropy fail on sequence", tnum)
+		}
+
+		if !sliceEql(entrpy, x.gapNotChar) {
+			t.Fatal("set ", tnum, "gapnochar wanted\n", x.gapNotChar, "got\n", entrpy)
+		}
+	}
+}
+
+// TestFindNdx
+func TestFindNdx(t *testing.T) {
+	set1 := `>should be in 0
+ABC
+>   some stuff here for seq1
+DEF
+> more here in seq2
+DEF`
+	s_opts := &Options{
+		Vbsty: 0, Keep_gaps_rd: true,
+		Dry_run:      true,
+		Rmv_gaps_wrt: false}
+	var tmpname string
+	var err error
+	var seqgrp SeqGrp
+	if tmpname, err = wrtTmp(set1); err != nil {
+		t.Fatal("tempfile error:", err)
+	}
+	defer os.Remove(tmpname)
+	if seqgrp, _, err = Readfile(tmpname, s_opts); err != nil {
+		t.Fatalf("Reading temp sequences %v", err)
+	}
+	subs := []string{"> some", " some", "some", "seq1"}
+	for _, s := range subs {
+		if n := seqgrp.FindNdx(s); n != 1 {
+			t.Fatalf("substring fail looking for %s, expected 1, got %d", s, n)
+		}
+	}
+	if n := seqgrp.FindNdx("this string is nowhere"); n != -1 {
+		t.Fatal("Failed with this string is nowhere")
+	}
+	if n := seqgrp.FindNdx("in seq2"); n != 2 {
+		t.Fatal("Failed lookin in seq2")
+	}
 }
