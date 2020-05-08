99aa6e9d0aa49a586efa00793993640921df4bc4
Andrew E. Torda <torda@zbh.uni-hamburg.de>
2020-05-08 10:51:33 +0200

    Common definitions for other programs. Things like exit codes.

---
+++ seq/common/common.go
@@ -0,0 +1,34 @@
+// 29 Apr 2020
+
+package common
+
+import (
+	"io"
+	"io/ioutil"
+	"fmt"
+)
+
+const (
+	ExitSuccess = iota
+	ExitFailure
+	ExitUsageError
+)
+
+const GapChar byte = '-'   // a minus sign is always used for gaps
+
+
+// WrtTemp writes a string to a temporary file and returns
+// the filename. It is used all over the place in testing.
+func WrtTemp(s string) (string, error) {
+	f_tmp, err := ioutil.TempFile("", "_del_me_testing")
+	if err != nil {
+		return "", fmt.Errorf("tempfile fail")
+	}
+
+	if _, err := io.WriteString(f_tmp, s); err != nil {
+		return "", fmt.Errorf("writing string to temp file %v", f_tmp.Name())
+	}
+	name := f_tmp.Name()
+	f_tmp.Close()
+	return name, nil
+}
