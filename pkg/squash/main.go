85b7a88f7544eeb50211b61bb271111f08d0bf17
Andrew E. Torda <torda@zbh.uni-hamburg.de>
2020-05-08 10:39:06 +0200

    Program for squashing alignments given some reference.

---
+++ pkg/squash/main.go
@@ -0,0 +1,65 @@
+// 29 April 2020
+
+package squash
+
+import (
+	"fmt"
+	"github.com/andrew-torda/goutil/seq"
+	. "github.com/andrew-torda/goutil/seq/common"
+	"os"
+)
+
+// MyMain is the top level main, after parsing the command line.
+func MyMain(seqstring, infile, outfile string) int {
+	s_opts := &seq.Options{
+		Vbsty: 0, Keep_gaps_rd: true,
+		Dry_run:      false,
+		Rmv_gaps_wrt: false}
+
+	seqgrp, _, err := seq.Readfile(infile, s_opts)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return ExitFailure
+	}
+	var ndxref int
+	if ndxref = seqgrp.FindNdx(seqstring); ndxref == -1 {
+		fmt.Fprintf(os.Stderr, "Could not find \"%s\" amongst sequences\n", seqstring)
+		return ExitFailure
+	}
+	seqslc := seqgrp.GetSeqSlc()       // Slice of sequences
+	maskseq := seqslc[ndxref].GetSeq() // The reference sequence
+	nfullseq := len(maskseq)
+	mask := make([]bool, nfullseq)
+	for i := range mask { // First, we assume all sites are interesting
+		mask[i] = true
+	}
+	for i, c := range maskseq { // Then we pick out the few that
+		if c == GapChar { //       should be taken out.
+			mask[i] = false
+		}
+	}
+	const emsg = "Length mismatch ref: %d seq %d len %d\n"
+	for i, ss := range seqgrp.GetSeqSlc() {
+		if len(ss.GetSeq()) != nfullseq {
+			fmt.Fprintf(os.Stderr, emsg, nfullseq, i, len(ss.GetSeq()))
+			return ExitFailure
+		}
+		b := ss.GetSeq()[:0]
+		for i, c := range ss.GetSeq() {
+			if mask[i] {
+				b = append(b, c)
+			}
+		}
+		seqgrp.GetSeqSlc()[i].SetSeq(b) // do not use "ss" here
+	}
+
+	if err := seq.Write_to_f(outfile, seqgrp.GetSeqSlc(), s_opts); err != nil {
+		if outfile == "" {
+			outfile = "os.Stdout"
+		}
+		fmt.Fprintln(os.Stderr, "Fail writing to ", outfile, err)
+		return ExitFailure
+	}
+
+	return ExitSuccess
+}
---
+++ pkg/squash/squash_test.go
@@ -0,0 +1,94 @@
+ // 29 Apr 2020
+
+package squash_test
+
+import (
+	"os"
+	"io/ioutil"
+	"log"
+	"testing"
+
+	"github.com/andrew-torda/goutil/seq/common"
+	. "github.com/andrew-torda/seq_compat/pkg/squash"
+)
+
+var seqstring string = `>s1
+ABCD
+> s2
+-EFG
+> s3
+-HIJ`
+
+//func TestMain1(t *testing.T) {
+func ExampleOut () {
+	fname, err := common.WrtTemp(seqstring)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.Remove(fname)
+	if MyMain("s2", fname, "") != common.ExitSuccess {
+		log.Fatal("broke running squash main")
+	}
+// Output:
+//>s1
+//BCD
+//> s2
+//EFG
+//> s3
+//HIJ
+}
+
+func TestWithOutput (t *testing.T) {
+	fname, err := common.WrtTemp(seqstring)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(fname)
+	tfile, err := ioutil.TempFile ("", "_del_me_testing")
+	outfname := tfile.Name()
+	tfile.Close()
+	if (err != nil) {
+		t.Fatal("cannot make temp filename", err)
+	}
+	r := MyMain("s2", fname, outfname)
+	defer os.Remove(outfname)
+	if r != common.ExitSuccess {
+		t.Fatal("broke running squash main")
+	}
+	if fi, err := os.Stat (outfname); err != nil {
+		t.Fatal("stat failed", err)
+	} else {
+		const sOf = "size of output from MyMain is too"
+		if fi.Size() < 25 {
+			t.Fatal(sOf, "small")
+		}
+		if fi.Size() > 27 {
+			t.Fatal(sOf, "big")
+		}
+	}
+}
+
+// TestBreak checks that we get an error if sequences have
+// wrong lengths
+func TestBreak(t *testing.T) {
+	var seqstring string = `>s1
+ABCD
+> s2
+-EF
+> s3
+-HIJ`
+
+	fname, err := common.WrtTemp(seqstring)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(fname)
+	old := os.Stderr // We provoke an error, so temporarily redirect stderr.
+	os.Stderr, err = os.OpenFile(os.DevNull, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if (err != nil) {t.Fatal("fail opening", os.DevNull)}
+	if MyMain("s2", fname, "") != common.ExitFailure {
+		os.Stderr = old
+		t.Fatal("broke running squash main")
+	}
+	os.Stderr = old
+}
