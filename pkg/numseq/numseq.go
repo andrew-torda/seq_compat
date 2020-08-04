4117e37b94c7230a99f74289fe420556b04406c4
Andrew E. Torda <torda@zbh.uni-hamburg.de>
2020-08-04 09:26:52 +0200

    Working. Before testing mmap.

--- pkg/numseq/numseq.go
+++ pkg/numseq/numseq.go
@@ -4,41 +4,84 @@ package numseq
 
 import (
 	"bytes"
-	"io"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"os"
 )
 
+// byreadFile is half the speed of the buffered versions.
+// Try a version which gets the size, allocates a buffer and reads
+// in one slurp.
+func byReadFile (fname string, bufsizeignored int) (int, error) {
+	if buf, err := ioutil.ReadFile (fname); err != nil {
+		return 0, err
+	} else {
+		return bytes.Count (buf, []byte(">")), nil
+	}
+}
 
-func byreading(fname string) (int, error) {
+// byOneSlurp will look at the size of the file, allocate a buffer and get the file
+// in one slurp. It turns out to be as slow as using the library Readfile().
+func byOneSlurp (fname string) (int, error) {
+	var fp *os.File
+	var sz int64
+	var err error
+	if fp, err = os.Open(fname); err != nil {
+		return 0, err
+	} else {
+		defer fp.Close()
+		if fi, err := fp.Stat(); err != nil {
+			return 0, err
+		}
+		sz = fi.Size()
+	}
+
+	buf := make ([]byte, sz)
+	if _, err := fp.Read (buf) ; err != nil {
+		return 0, err}
+
+	return bytes.Count (buf, []byte(">")), nil
+}
+
+	
+func byreadingFixed (fname string, bufsizeignored int) (int, error) {
+	const bsize = 100 * 1024
+	var buf [bsize]byte
+	return (innerbyreading (fname, buf[:]))
+}
+
+func byreadingVaries (fname string, bufsize int) (int, error) {
+	buf := make ([]byte, bufsize)
+	return (innerbyreading (fname, buf))
+}
+
+
+func innerbyreading(fname string, buf []byte) (int, error) {
 	var fp io.ReadCloser
 	var err error
-	if fp, err = os.Open (fname); err != nil {
+	if fp, err = os.Open(fname); err != nil {
 		return 0, err
 	}
 	defer fp.Close()
-	const bsize = 1
-	var local [bsize]byte
-	var buf []byte = local[:]
-	n := bsize
 	count := 0
-	for  ; n == bsize; {
-		n, err = fp.Read (buf)
+	bsize := len (buf)
+	for n := bsize; n == bsize; {
+		n, err = fp.Read(buf[:])
 		if err != nil && err != io.EOF {
 			return 0, err
 		}
-		count += bytes.Count (buf[:n], []byte(">"))
+		count += bytes.Count(buf[:n], []byte(">"))
 	}
 
-	count += bytes.Count (buf[:n], []byte(">"))
 	return count, nil
 }
 
-func Main (fname string) (int, error) {
+func Main(fname string) (int, error) {
 	if _, err := os.Stat(fname); os.IsNotExist(err) {
 		fmt.Println(err)
 	}
 	var tmp [100]byte
-	print (tmp[0:20])
+	print(tmp[0:20])
 	return 0, nil
 }
