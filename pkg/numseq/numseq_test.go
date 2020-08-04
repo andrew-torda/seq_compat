21c1875ccab3ad46fb579da050cbce6b0242a44c
Andrew E. Torda <torda@zbh.uni-hamburg.de>
2020-08-04 13:39:54 +0200

    Testing is now over a slice of functions.
    Benchmarking is now much shortened by putting everything into dobench() and calling the two remaining alternatives via function pointers.

--- pkg/numseq/numseq_test.go
+++ pkg/numseq/numseq_test.go
@@ -31,21 +31,25 @@ func makeTestData() (string, error) {
 	return f_tmp.Name(), nil
 }
 
-func TestByreading (t *testing.T) {
+func TestCount (t *testing.T) {
 	var fname string
 	var err error
 	if fname, err = makeTestData(); err != nil {
 		t.Fatal(err)
 	}
 	defer os.Remove (fname)
-	if i, err := numseq.ByreadingFixed (fname, 1); i != smalltestArg.Nseq {
-		t.Fatal ("Expected", smalltestArg.Nseq, "got", i)
-	} else if err != nil {
-		t.Fatal (err)
+	type nfunc func (string) (int, error)
+	for _, ff := range []nfunc {numseq.ByReadingFixed, numseq.ByMmap, numseq.Main} {
+		if i, err := ff (fname); i != smalltestArg.Nseq {
+			t.Fatal ("Expected", smalltestArg.Nseq, "got", i)
+		} else if err != nil {
+			t.Fatal (err)
+		}
 	}
 }
 
-func setupbmark (b *testing.B) (string, int) {
+type fToBench func (string) (int, error)
+func dobench (b *testing.B, f fToBench) {
 	b.StopTimer()
 	var fname string
 	var err error
@@ -54,55 +58,16 @@ func setupbmark (b *testing.B) (string, int) {
 	}
 	b.Cleanup (func () {os.Remove(fname)})
 	b.StartTimer()
-	return fname, smalltestArg.Nseq
-}	
-
-func BenchmarkByreadingFixed(b *testing.B) {
-	fname, nset := setupbmark(b)
-    i, _ := numseq.ByreadingFixed (fname, 1)
-	if i != nset {
+    i, _ := f (fname)
+	if i != smalltestArg.Nseq	 {
 		b.Fatal ("Expected", smalltestArg.Nseq, "got", i)
 	}
 }
 
-func BenchmarkByreadingVary20k(b *testing.B) {
-	fname, nset := setupbmark(b)
-    i, _ := numseq.ByreadingVaries (fname, 20 * 1024)
-	if i != nset {
-		b.Fatal ("Expected", nset, "got", i)
-	}
-}
-
-func BenchmarkByreadingVary50k(b *testing.B) {
-	fname, nset := setupbmark(b)
-    i, _ := numseq.ByreadingVaries (fname, 50 * 1024)
-	if i != nset {
-		b.Fatal ("Expected", nset, "got", i)
-	}
-}
-
-func BenchmarkByreadingVary100k(b *testing.B) {
-	fname, nset := setupbmark(b)
-    i, _ := numseq.ByreadingVaries (fname, 100 * 1024)
-	if i != nset {
-		b.Fatal ("Expected", nset, "got", i)
-	}
-}
-
-
-func BenchmarkByreadingVary1m(b *testing.B) {
-	fname, nset := setupbmark(b)
-    i, _ := numseq.ByreadingVaries (fname, 1024 * 1024)
-	if i != nset {
-		b.Fatal ("Expected", nset, "got", i)
-	}
-}
-
 func BenchmarkByMmap (b *testing.B) {
-	fname, nset := setupbmark(b)
-    i, _ := numseq.ByMmap (fname)
-	if i != nset {
-		b.Fatal ("Expected", nset, "got", i)
-	}
+    dobench (b, numseq.ByMmap)
 }
 
+func BenchmarkByReadingFixed (b *testing.B) {
+    dobench (b, numseq.ByReadingFixed)
+}
