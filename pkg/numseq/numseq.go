cd1cafd97d4f0eb2217ef652542ae0c4f07d7839
Andrew E. Torda <torda@zbh.uni-hamburg.de>
2020-08-04 12:00:50 +0200

    After benchmarking, a version with memory mapping is the fastest.
    Using a fixed buffer on the stack does not bring much. Reading the optimisation output, it gets moved to the heap anyway.
    The optimal size of the buffer is somewhere between 50 and 100 time 1024.
    The surprise is that reading the file in in slurp is slowest, regardless of whether one uses the library ReadFile() or a self-implemented version.
    BenchmarkByreadingFixed-4      	1000000000	         0.00341 ns/op
    BenchmarkByreadingVary20k-4    	1000000000	         0.00377 ns/op
    BenchmarkByreadingVary50k-4    	1000000000	         0.00346 ns/op
    BenchmarkByreadingVary100k-4   	1000000000	         0.00335 ns/op
    BenchmarkByreadingVary1m-4     	1000000000	         0.00361 ns/op
    BenchmarkByReadFile-4          	1000000000	         0.00883 ns/op
    BenchmarkByOneSlurp-4          	1000000000	         0.00859 ns/op
    BenchmarkByMmap-4              	1000000000	         0.00289 ns/op

--- pkg/numseq/numseq.go
+++ pkg/numseq/numseq.go
@@ -8,22 +8,40 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
+
+	"github.com/edsrzf/mmap-go"
 )
 
-// byreadFile is half the speed of the buffered versions.
+// byMmap
+func byMmap(fname string) (int, error) {
+	var fp *os.File
+	var err error
+	var mm mmap.MMap
+	if fp, err = os.Open(fname); err != nil {
+		return 0, err
+	}
+	defer fp.Close()
+	if mm, err = mmap.Map(fp, mmap.RDONLY, 0); err != nil {
+		return 0, err
+	}
+	defer mm.Unmap()
+	return bytes.Count(mm, []byte(">")), nil
+}
+
+// byReadFile is half the speed of the buffered versions.
 // Try a version which gets the size, allocates a buffer and reads
 // in one slurp.
-func byReadFile (fname string, bufsizeignored int) (int, error) {
-	if buf, err := ioutil.ReadFile (fname); err != nil {
+func byReadFile(fname string, bufsizeignored int) (int, error) {
+	if buf, err := ioutil.ReadFile(fname); err != nil {
 		return 0, err
 	} else {
-		return bytes.Count (buf, []byte(">")), nil
+		return bytes.Count(buf, []byte(">")), nil
 	}
 }
 
 // byOneSlurp will look at the size of the file, allocate a buffer and get the file
 // in one slurp. It turns out to be as slow as using the library Readfile().
-func byOneSlurp (fname string) (int, error) {
+func byOneSlurp(fname string) (int, error) {
 	var fp *os.File
 	var sz int64
 	var err error
@@ -33,29 +51,29 @@ func byOneSlurp (fname string) (int, error) {
 		defer fp.Close()
 		if fi, err := fp.Stat(); err != nil {
 			return 0, err
+		} else {
+			sz = fi.Size()
 		}
-		sz = fi.Size()
 	}
 
-	buf := make ([]byte, sz)
-	if _, err := fp.Read (buf) ; err != nil {
-		return 0, err}
+	buf := make([]byte, sz)
+	if _, err := fp.Read(buf); err != nil {
+		return 0, err
+	}
 
-	return bytes.Count (buf, []byte(">")), nil
+	return bytes.Count(buf, []byte(">")), nil
 }
 
-	
-func byreadingFixed (fname string, bufsizeignored int) (int, error) {
-	const bsize = 100 * 1024
+func byreadingFixed(fname string, bufsizeignored int) (int, error) {
+	const bsize = 64 * 1024
 	var buf [bsize]byte
-	return (innerbyreading (fname, buf[:]))
+	return (innerbyreading(fname, buf[:]))
 }
 
-func byreadingVaries (fname string, bufsize int) (int, error) {
-	buf := make ([]byte, bufsize)
-	return (innerbyreading (fname, buf))
+func byreadingVaries(fname string, bufsize int) (int, error) {
+	buf := make([]byte, bufsize)
+	return (innerbyreading(fname, buf))
 }
-
 
 func innerbyreading(fname string, buf []byte) (int, error) {
 	var fp io.ReadCloser
@@ -65,7 +83,7 @@ func innerbyreading(fname string, buf []byte) (int, error) {
 	}
 	defer fp.Close()
 	count := 0
-	bsize := len (buf)
+	bsize := len(buf)
 	for n := bsize; n == bsize; {
 		n, err = fp.Read(buf[:])
 		if err != nil && err != io.EOF {
