5213f257f85a9dff0d9fcf59e10b1b61ddd7f3ad
Andrew E. Torda <torda@zbh.uni-hamburg.de>
2020-08-09 17:18:50 +0200

    Added use of a sync pool for the items. The lexer function creates items or has them created by Get(). One of the two consuming functions put the item back in the pool when they are finished.

--- pkg/seq/readseqs2.go
+++ pkg/seq/readseqs2.go
@@ -5,6 +5,7 @@ package seq
 import (
 	"bytes"
 	"io"
+	"sync"
 )
 
 // An item is terminated by a newline if we are in a comment or a comment
@@ -20,14 +21,15 @@ type item struct {
 }
 
 type lexer struct {
-	input  []byte
-	ichan  chan *item
-	seqgrp *SeqGrp
-	rdr    io.Reader
-	cmmt   string // partial comment
-	seq    []byte // partial string
-	term   byte
-	err    error
+	input    []byte
+	ichan    chan *item
+	seqgrp   *SeqGrp
+	rdr      io.Reader
+	itempool sync.Pool
+	cmmt     string // partial comment
+	seq      []byte // partial string
+	term     byte
+	err      error
 }
 
 const defaultReadSize = 512
@@ -37,12 +39,16 @@ var rdsize int = defaultReadSize
 // setFastaRdSize is only used during benchmarking
 func setFastaRdSize(i int) { rdsize = i }
 
+func newItem() interface{} { return new(item) }
+
 // next reads from the input and sends an item to channel, ichan.
 // An item is terminated by l.term, or the end of the buffer or
 // end of input.
 func (l *lexer) next() {
+	l.itempool.New = newItem
+	l.rdBufpool.New = newRdBuf
 	for {
-		item := new(item)
+		item := l.itempool.Get().(*item)
 		if len(l.input) == 0 {
 			l.input = make([]byte, rdsize)
 			if n, err := l.rdr.Read(l.input); n != rdsize {
@@ -93,6 +99,7 @@ func removeWhite(s []byte) []byte {
 // We are reading a sequence
 func gseq(l *lexer) stateFn {
 	item := <-l.ichan
+	defer l.itempool.Put(item)
 	if item == nil || l.err != nil {
 		return nil
 	}
@@ -111,6 +118,7 @@ func gseq(l *lexer) stateFn {
 // We are reading a comment
 func gcmmt(l *lexer) stateFn {
 	item := <-l.ichan
+	defer l.itempool.Put(item)
 	if item == nil || l.err != nil {
 		return nil
 	}
@@ -125,7 +133,7 @@ func gcmmt(l *lexer) stateFn {
 
 // Readfasta is my second version of a fasta reader.
 func ReadFasta(rdr io.Reader, seqgrp *SeqGrp, s_opts *Options) (err error) {
-	l := lexer{rdr: rdr, ichan: make(chan *item), seqgrp: seqgrp, term: NL}
+	l := lexer{rdr: rdr, ichan: make(chan *item, 2), seqgrp: seqgrp, term: NL}
 
 	go l.next()
 	for state := gcmmt; state != nil; {
