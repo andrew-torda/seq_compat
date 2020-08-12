5ae4640e3d263395c922e63b90cb5d62f3a1d1bb
andrew-torda <55841222+andrew-torda@users.noreply.github.com>
2020-08-12 12:24:53 +0200

    Check three different versions of the white removal function.

--- pkg/white/white_test.go
+++ pkg/white/white_test.go
@@ -2,28 +2,47 @@
 package white_test
 
 import (
-	. "github.com/andrew-torda/seq_compat/pkg/white"
 	"testing"
+
+	. "github.com/andrew-torda/seq_compat/pkg/white"
 )
 
+type ftest func(*[]byte)
+
 // TestWhiteRemove
-func TestWhiteRemove(t *testing.T) {
+func testoneversion(f ftest, t *testing.T) {
+
 	want := "abcdefghijk"
 	ss := []string{
 		want,
+		"abcdefghijk ",
+		"abcdefghijk  ",
+		"abcdefghij k ",
+		"abcdefghij  k ",
+		"abcdefghij  k  ",
+		" abcdefghij  k  ",
+		"  abcdefghij  k  ",
+		" a bcdefghij  k  ",
 		" a b c d e f g h i j k",
 		"a b c de fgh ijk",
 		"   abcdefghijk    ",
 		"a   b      cdefghijk\n ",
 		"a  b  c  d   e    f     ghijk",
 		"a bcdefghij   k",
-		"abcdefghij\nk",
+		"abcdefghij\tk",
 	}
 	for i, s := range ss {
 		b := []byte(s)
-		Remove(&b)
+		f(&b)
 		if string(b) != want {
 			t.Fatalf("white remove broke on \"%s\" got \"%s\"", string(s), ss[i])
 		}
+	}
+}
+
+func TestWhiteRemove(t *testing.T) {
+	fws := []ftest{Remove, RemoveByBlock, RemoveByFields}
+	for _, f := range fws {
+		testoneversion(f, t)
 	}
 }
