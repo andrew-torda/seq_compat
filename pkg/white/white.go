a103bb90ce2a5faa678a682161d683a0d20c40b6
andrew-torda <55841222+andrew-torda@users.noreply.github.com>
2020-08-13 16:30:08 +0200

    Another version which copies blocks, but avoids two if statements inside the main loop.

--- pkg/white/white.go
+++ pkg/white/white.go
@@ -4,6 +4,7 @@ package white
 
 import (
 	"bytes"
+	"fmt"
 )
 
 // WhiteRemove acts on a byte slice, in place and removes all the white
@@ -68,6 +69,49 @@ func removeByBlock(sIn *[]byte) {
 		s = s[:dst]
 	}
 	*sIn = s
+}
+
+func RemoveWithGoTo(sIn *[]byte) {
+	var asciiSpace = [256]bool{
+		0: true, '\t': true, '\n': true, '\v': true, '\f': true, '\r': true, ' ': true,
+	}
+
+	s := *sIn
+	fmt.Printf("In: \"%s\".. ", string(s))
+	var start, dst int
+
+	i := 0
+
+	mustclean := false
+white:
+	for i < len(s) {
+
+		for ; i < len(s); i++ {
+			if !asciiSpace[s[i]] {
+				start = i
+				break
+			}
+		}
+
+		for ; i < len(s); i++ {
+			mustclean = true
+			if asciiSpace[s[i]] { // end of block
+				ltmp := i - start // this is the correct length
+				copy(s[dst:dst+ltmp], s[start:i+1])
+				dst = dst + ltmp
+				mustclean = false
+				continue white
+			}
+		}
+	}
+	if mustclean { // hit end of input, so spit out leftovers
+		ltmp := len(s) - start
+		s = append(s[:dst], s[start:start+ltmp]...)
+	} else { // If we finished in white space, we have to truncate
+		s = s[:dst]
+	}
+	*sIn = s
+	fmt.Printf("out: \"%s\"\n", string(s))
 }
 
 // RemoveByFields does the same, but using a library call
