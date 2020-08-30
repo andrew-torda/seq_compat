43b8ea496bfc4c1a23ad3decef758b0e0774e6e4
Andrew E. Torda <torda@zbh.uni-hamburg.de>
2020-08-30 16:41:33 +0200

    Updated flag descriptions.

--- cmd/randseq/doc.go
+++ cmd/randseq/doc.go
@@ -3,12 +3,25 @@
 /*
 
 Randseq is for making random sequences for testing the code.
+Usage:
+	randseq [options] nseq length
+will generate nseq sequences of length length.
+
+Flags:
+	-g
+		no gaps in the output sequences
+	-e
+		provoke errors. Sequences will not be the same length. This is an error
+		for programs like entropy.
+	-r
+		random number seed
+
 We are most interested in benchmarking and parsing, so the content is not so important.
 The only question that comes up is white space and gaps.
 Whitespace should generally be unpredictable, so we encounter funny cases.
 There is a flag which tells us whether we have gaps.
-We should add a flag to generate sequences of different lengths, so as to
-provoke errors.
+Most programs we use expect sequences to be the same length (after gap removal),
+so we can provoke errors and have different lengths.
 
 There are two main entry points.
  1. Write to a file
