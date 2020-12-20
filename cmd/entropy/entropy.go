79ad490f0ff533be895536a66fa0a7e6932c8d5c
Andrew E. Torda <torda@zbh.uni-hamburg.de>
2020-12-20 16:32:42 +0100

    Documented -c option. Tried to make some other aspects clearer.

--- cmd/entropy/entropy.go
+++ cmd/entropy/entropy.go
@@ -15,17 +15,22 @@ import (
 
 func usage() {
 	fmt.Fprintln(os.Stderr, "usage:", path.Base(os.Args[0]), "[infile [outfile]]")
-	fmt.Fprintln(os.Stderr, "Do not just type the command name. It will wait on input from stdin")
+	long := `Do not just type type command name. It will wait on input from stdin.
+Given no arguments, read and write from stdin / stdout.
+Given one argument, read from the given file name, but write to stdout.
+Given two arguments, read from the first one, write to the second.`
+	fmt.Fprintln(os.Stderr, long)
 	flag.PrintDefaults()
 }
 
 func main() {
 	var flags entropy.CmdFlag
 	var infile, outfile string
+
+    flag.StringVar (&flags.Chimera, "c", "", "filename to write chimera format to")
 	flag.IntVar(&flags.Offset, "f", 0, "offset for numbering output, renumbering sites")
 	flag.BoolVar(&flags.GapsAreChar, "g", false, "gap is a valid symbol")
 	flag.IntVar(&flags.NSym, "n", -1, "num symbols, guessed by default, 4 for DNA")
-	flag.StringVar(&outfile, "o", "", "output file name, default stdout")
 	flag.StringVar(&flags.RefSeq, "r", "", "reference sequence, check compatibility")
 	flag.BoolVar(&flags.Time, "t", false, "print out timing information")
     flag.Usage = usage
