ce0bd55f6f1ea68bdba6faec8f69bd53890f9768
Andrew E. Torda <andrew.torda@uni-hamburg.de>
2025-05-28 16:05:38 +0200

    This will be for counting sequence lengths.

---
+++ pkg/seqlen/seqlen.go
@@ -0,0 +1,18 @@
+// 15 May 2025
+// We are given a multiple sequence alignment.
+// For each sequence, get the comment, species name and write send
+// create a file for a spreadsheet.
+// This will have the gene_id, the species name and the length
+// of the sequence without gaps.
+
+package seqlen
+
+const (
+        ok byte = iota
+        unhappy
+        fatal
+        stopping
+)
+func Mymain (flags *CmdFlag, infile, outfile string) error {
+	return nil
+}
